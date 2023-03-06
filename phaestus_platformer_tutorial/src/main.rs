7cd3dd2ea7c775d8059b800f50bcaef3fc993762
Alan Tran-Kiem <alan001tk@gmail.com>
2023-03-05 23:32:31 -0800

    Start work on changing player animations

--- phaestus_platformer_tutorial/src/main.rs
+++ phaestus_platformer_tutorial/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         .add_startup_system(spawn_cam)
         .add_startup_system(spawn_player)
         .add_system(animate_sprite)
+        .add_system(move_player)
         .run();
 }
 
@@ -71,5 +72,44 @@ fn animate_sprite(
             }
             frame_time.0 -= animation.frame_time * frames as f32;
         }
+    }
+}
+
+const MOVE_SPEED: f32 = 100.;
+
+fn move_player(
+    mut player: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+    input: Res<Input<KeyCode>>,
+) {
+    let mut player = player.single_mut();
+    if input.any_pressed([KeyCode::A, KeyCode::Left]) {
+        player.translation.x -= MOVE_SPEED * time.delta_seconds();
+    } else if input.any_pressed([KeyCode::D, KeyCode::Right]) {
+        player.translation.x += MOVE_SPEED * time.delta_seconds();
+    }
+}
+
+fn change_player_animation(
+    mut player: Query<
+        (
+            &mut Handle<TextureAtlas>,
+            &mut SpriteAnimation,
+            &mut TextureAtlasSprite,
+        ),
+        With<Player>,
+    >,
+    input: Res<Input<KeyCode>>,
+    mut texture_atlas: ResMut<Assets<TextureAtlas>>,
+    asset_server: Res<AssetServer>,
+) {
+    let (mut atlas, mut animation, mut sprite) = player.single_mut();
+    if input.any_just_pressed([KeyCode::A, KeyCode::Left, KeyCode::D, KeyCode::Right]) {
+        // Set walk animation
+    }
+    if input.any_just_released([KeyCode::A, KeyCode::Left, KeyCode::D, KeyCode::Right])
+        && !input.any_pressed([KeyCode::A, KeyCode::Left, KeyCode::D, KeyCode::Right])
+    {
+        // Set idle animation
     }
 }
