eba3bdfeb1ee284b86ec0e466a2b7b0709811ee8
= <=>
2023-03-25 21:52:38 -0700

    Added ordering to player systems

--- jacques_tutorial/bevy-ball-game/src/player.rs
+++ jacques_tutorial/bevy-ball-game/src/player.rs
@@ -4,13 +4,21 @@ mod systems;
 
 use systems::*;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player)
-            .add_system(player_movement)
-            .add_system(confine_player_movement)
+        app.configure_set(MovementSystemSet.before(ConfinementSystemSet))
+            .add_startup_system(spawn_player)
+            .add_systems((player_movement, confine_player_movement).chain())
+            .add_system(player_movement.in_set(MovementSystemSet))
+            .add_system(confine_player_movement.in_set(ConfinementSystemSet))
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
     }
